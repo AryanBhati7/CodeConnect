549036bae04b144e9172f22d3214c9038d204a36
Aryan Bhati <aryanbhati9638@gmail.com>
2024-07-14 18:28:37 +0530

    frontend setup

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -70,7 +70,6 @@ userSchema.pre('save', async function (next) {
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-  //returns true or false
   return await bcrypt.compare(password, this.password);
 };
 
